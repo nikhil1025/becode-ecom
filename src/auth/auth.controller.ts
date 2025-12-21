98121e8e18e045ac2a2ae496087fcc05a72f848a
nikhil1025 <nikhilshrivastava1025@gmail.com>
2025-12-21 23:58:34 +0530

    S3 region fixed

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -18,8 +18,8 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 
 type UserWithoutPassword = Omit<User, 'password'>;
 
-const FRONTEND_URL = process.env.FRONTEND_URL || 'http://13.232.186.110:3000';
-// const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+// const FRONTEND_URL = process.env.FRONTEND_URL || 'http://13.232.186.110:3000';
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
 
 @Controller('auth')
 export class AuthController {
