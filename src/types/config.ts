b0b2f2e2ba37fc9992da2df8507160137f23add5
nikhil1025 <nikhilshrivastava1025@gmail.com>
2025-12-26 20:06:30 +0530

    variant style updatd

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,6 +1,6 @@
-// export const FRONTEND_URL =
-//   process.env.FRONTEND_URL || 'http://13.232.186.110:3000';
-//   export const BASE_URL = process.env.BASE_URL || 'http://65.2.146.90:3001';
+export const FRONTEND_URL =
+  process.env.FRONTEND_URL || 'http://13.232.186.110:3000';
+  export const BASE_URL = process.env.BASE_URL || 'http://65.2.146.90:3001';
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
-export const BASE_URL = process.env.BASE_URL || 'http://localhost:3001';
+// export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+// export const BASE_URL = process.env.BASE_URL || 'http://localhost:3001';
